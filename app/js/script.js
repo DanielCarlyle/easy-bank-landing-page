cf9cb4cfb94adaf3ac9cdf75e47852941402025b
Daniel <dan.carlyle@gmail.com>
2021-05-10 11:09:44 -0400

    added hamburger menu and nav content

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+//console.log('hello world');
+
+const btnHamburger = document.querySelector("#btnHamburger");
+
+btnHamburger.addEventListener('click', function() {
+  console.log("open Hamburger");
+
+  if (btnHamburger.classList.contains('open')) {
+      btnHamburger.classList.remove('open');
+  } else {
+    btnHamburger.classList.add('open');
+  }
+)
+});
